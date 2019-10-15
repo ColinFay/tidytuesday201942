b020dce1cd951c036556e6b337fe69463999ff60
colin <colin@thinkr.fr>
2019-10-15 22:41:31 +0200

    added title tag

--- R/app_ui.R
+++ R/app_ui.R
@@ -66,6 +66,7 @@ golem_add_external_resources <- function(){
   tags$head(
     golem::activate_js(),
     golem::favicon(),
+    tags$title("TidyTuesday 2019 - 42"),
     # Add here all the external resources
     # If you have a custom.css in the inst/app/www
     # Or for example, you can add shinyalert::useShinyalert() here
